7b16ddb781930a1e394d1509022833f6c28436d5
Laurentiu Soica <laurentiu.soica@spirent.com>
2024-10-05 12:14:49 +0300

    fix: gh wf not used
    Signed-off-by: Laurentiu Soica <laurentiu.soica@spirent.com>

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728119158',resources: [
+    cacheName: 'chirpy-1728119685',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
