c2c634e5f028515fc43d2d47009f1ecf3c65f14c
Laurentiu Soica <laurentiu.soica@spirent.com>
2024-10-07 17:30:44 +0300

    fix: link to google colab
    Signed-off-by: Laurentiu Soica <laurentiu.soica@spirent.com>

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728311369',resources: [
+    cacheName: 'chirpy-1728311428',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
