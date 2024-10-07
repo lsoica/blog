90b782537473f2e533fc10a8aed1f426af838515
Laurentiu Soica <laurentiu.soica@spirent.com>
2024-10-07 17:31:52 +0300

    fix: remove pin
    Signed-off-by: Laurentiu Soica <laurentiu.soica@spirent.com>

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728311428',resources: [
+    cacheName: 'chirpy-1728311502',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
