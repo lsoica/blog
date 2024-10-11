bd7bbd1be18206ced6d9673c43c8f40ebed5b2cc
Laurentiu Soica <laurentiu.soica@spirent.com>
2024-10-11 13:56:17 +0300

    fix: pretty print
    Signed-off-by: Laurentiu Soica <laurentiu.soica@spirent.com>

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728644067',resources: [
+    cacheName: 'chirpy-1728644171',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
