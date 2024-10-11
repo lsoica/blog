d8af5a2f027877dea53970eb2087dd6f501bc226
Laurentiu Soica <laurentiu.soica@spirent.com>
2024-10-11 13:54:36 +0300

    fix: custom top k
    Signed-off-by: Laurentiu Soica <laurentiu.soica@spirent.com>

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728643975',resources: [
+    cacheName: 'chirpy-1728644067',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
