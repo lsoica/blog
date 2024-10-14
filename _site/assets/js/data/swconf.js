8ebb13c69ddab4729bb908ce750262bc6de6ef3e
Laurentiu Soica <laurentiu.soica@spirent.com>
2024-10-14 15:52:09 +0300

    feat: add MLP
    Signed-off-by: Laurentiu Soica <laurentiu.soica@spirent.com>

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728644859',resources: [
+    cacheName: 'chirpy-1728910244',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
