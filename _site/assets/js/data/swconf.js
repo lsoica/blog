493b3c7e9997d091a3c255bd4ae5b377b3b99fca
Laurentiu Soica <laurentiu.soica@spirent.com>
2024-10-17 19:29:36 +0300

    fix: remove local paths
    Signed-off-by: Laurentiu Soica <laurentiu.soica@spirent.com>

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728997698',resources: [
+    cacheName: 'chirpy-1729175710',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
