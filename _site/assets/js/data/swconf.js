aa1f586c8d404b218ffb03d1345d065065669acd
Laurentiu Soica <laurentiu.soica@spirent.com>
2024-10-17 15:57:44 +0300

    feat: more content
    Signed-off-by: Laurentiu Soica <laurentiu.soica@spirent.com>

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728995081',resources: [
+    cacheName: 'chirpy-1728997698',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
