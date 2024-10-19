d27c7748dc75358efa2104110919a80e58888f21
Laurentiu Soica <laurentiu.soica@spirent.com>
2024-10-19 11:26:38 +0300

    fix: update autoencoder
    Signed-off-by: Laurentiu Soica <laurentiu.soica@spirent.com>

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729315357',resources: [
+    cacheName: 'chirpy-1729326388',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
