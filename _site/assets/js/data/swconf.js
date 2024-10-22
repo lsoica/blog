e07b9f72d8abc210ec2aeb02a843596e16abe235
Laurentiu Soica <laurentiu.soica@spirent.com>
2024-10-22 11:17:07 +0300

    fix: add missing files
    Signed-off-by: Laurentiu Soica <laurentiu.soica@spirent.com>

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729326388',resources: [
+    cacheName: 'chirpy-1729584903',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
