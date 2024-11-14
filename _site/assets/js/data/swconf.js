a451d5598a66500ca9208082079731578d598ab5
Laurentiu Soica <laurentiu.soica@spirent.com>
2024-11-14 12:42:19 +0200

    fix: more changes

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731580851',resources: [
+    cacheName: 'chirpy-1731580921',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
