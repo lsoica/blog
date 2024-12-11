469ebf95d40a4e1e8283b0ec1acdf1d43883e589
Laurentiu Soica <laurentiu.soica@spirent.com>
2024-12-11 14:17:27 +0200

    fix: icons

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733919149',resources: [
+    cacheName: 'chirpy-1733919414',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
