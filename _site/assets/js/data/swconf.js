e0dc44a6f7dbab92391be544e8938099d5ecccd5
Laurentiu Soica <laurentiu.soica@spirent.com>
2025-04-11 18:07:08 +0300

    fix: update timestamps across multiple files

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744382834',resources: [
+    cacheName: 'chirpy-1744382862',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
